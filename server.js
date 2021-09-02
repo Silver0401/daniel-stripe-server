93f140ee9742bd203bdb1a19b469843d948da904
Ismael Muñoz <Prosilver05@gmail.com>
2021-09-02 14:31:58 -0500

    removed images from last proyecto and added GET method for stripe checkout

--- server.js
+++ server.js
@@ -1,33 +1,48 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+const stripe = require("stripe")
+require("dotenv").config()
 let port = process.env.PORT || 4000
-app.use(express.json())
 
-const missingList = [12, 23, 34, 45, 56]
+app.use(express.json())
+app.use(cors({ origin: true}))
+const stripeKey = stripe("sk_test_51I2XfOBwVvvXbSw3wKEvsjnWLq2VvChCW0agoiIwbjTakGMsUTzpclEu2Yl5h4Fxf3Pd3I1umjwNb6Vk3EZZkHtF00xXEHsa5Y")
+
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
-  
+  res.sendFile(__dirname + "/index.html")  
 })
 
+app.post("/create-checkout-session", async (req,res) => {
+	const domainURL = "http://localhost:3000"
+	const customer_email = req.body.customer_email
+	const line_items = req.body.line_items
 
-for (let c = 2; c <= 66; c++){
-  if (missingList.includes(c)){} 
-  else {
-    app.get(`/Diapositiva${c}`, (req,res) => {
+	if (!line_items || !customer_email){
+		return res.status(400).json({error: "Backend Error: missing parameters"})
+	}
+	let session;
 
-      res.sendFile(__dirname + `/resources/Diapos1/Diapositiva${c}.jpeg`)
+	try{
+		session = await stripeKey.checkout.sessions.create({
+			payment_method_types: ['card'],
+			line_items,
+			customer_email,
+			mode: 'payment',
+			success_url: `${domainURL}/success`,
+			cancel_url: `${domainURL}/canceled`
+		})
 
-    })
-  }
-}
+		res.status(200).json({sessionId: session.id})
 
-for (let c = 1; c <= 60; c++){
-  app.get(`/Diapo${c}`, (req,res) => {
-    res.sendFile(__dirname + `/resources/Diapos2/Diapo${c}.jpeg`)
-  })
-}
+	}
+	catch(error) {
+		console.log(error)
+		res.status(400).json({error: `Backend error: ${error}`})
+	}
 
+})
 
 
 app.listen(port, () => console.log(`App server initalized on port ${port}`))

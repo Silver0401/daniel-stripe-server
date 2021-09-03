b1844111cbadab85e09209b479e3d37a4280612e
Ismael Muñoz <Prosilver05@gmail.com>
2021-09-02 23:43:31 -0500

    added stripe checkout monthly function

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ let port = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(cors({ origin: true}))
-const stripeKey = stripe("sk_test_51I2XfOBwVvvXbSw3wKEvsjnWLq2VvChCW0agoiIwbjTakGMsUTzpclEu2Yl5h4Fxf3Pd3I1umjwNb6Vk3EZZkHtF00xXEHsa5Y")
+const stripeKey = stripe(process.env.STRIPE_KEY)
 
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")  
 })
 
-app.post("/create-checkout-session", async (req,res) => {
+app.post("/create-one-time-checkout-session", async (req,res) => {
 	const domainURL = "http://localhost:3000"
 	const customer_email = req.body.customer_email
 	const line_items = req.body.line_items
@@ -41,7 +41,35 @@ app.post("/create-checkout-session", async (req,res) => {
 		console.log(error)
 		res.status(400).json({error: `Backend error: ${error}`})
 	}
+})
 
+app.post("/create-monthly-checkout-session", async (req,res) => {
+	const domainURL = "http://localhost:3000"
+	const customer_email = req.body.customer_email
+	const line_items = req.body.line_items
+
+	if (!line_items || !customer_email){
+		return res.status(400).json({error: "Backend Error: missing parameters"})
+	}
+	let session;
+
+	try{
+		session = await stripeKey.checkout.sessions.create({
+			payment_method_types: ['card'],
+			line_items,
+			customer_email,
+			mode: 'subscription',
+			success_url: `${domainURL}/success`,
+			cancel_url: `${domainURL}/canceled`
+		})
+
+		res.status(200).json({sessionId: session.id})
+
+	}
+	catch(error) {
+		console.log(error)
+		res.status(400).json({error: `Backend error: ${error}`})
+	}
 })
 
 
